a3a50989ec96d0b4aa8356522081f528fa4b7742
Sergio Pomárico <sergiodavid21@gmail.com>
2017-06-03 16:39:17 -0500

    add admin sub pages

--- inc/functions.admin.php
+++ inc/functions.admin.php
@@ -15,13 +15,27 @@
  * add a custom admin page for the theme
  */
 function sunset_add_admin_page(){
+    // add menu page
     add_menu_page('Sunset Theme Options','Sunset','manage_options','sunset','sunset_create_admin_page', get_template_directory_uri().'/img/sunset-icon.png', 110);
-    // add_action();
+
+    // add general menu page (is the same page as the main menu)
+    add_submenu_page('sunset','Sunset Theme Options','General','manage_options','sunset','sunset_create_admin_page');
+
+    // add css menu page
+    add_submenu_page('sunset','Sunset CSS Options','Custom CSS','manage_options','sunset-css','sunset_theme_css_setting');
 }
 
 /**
  * generate admin page for the theme
  */
-function sunset_create_admin_page(){}
+function sunset_create_admin_page(){
+    require_once(get_template_directory().'/inc/templates/sunset-admin.php');
+}
 
+/**
+ * generate setting page for the theme
+ */
+function sunset_theme_css_setting(){}
+
+// load theme menu on admin wordpress hook
 add_action('admin_menu', 'sunset_add_admin_page');
