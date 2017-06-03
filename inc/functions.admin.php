0fed45e2f76b88495aa15558ff4e5da1a09bba98
Sergio Pomárico <sergiodavid21@gmail.com>
2017-06-03 18:47:12 -0500

    add comments and reorginze files

--- inc/functions.admin.php
+++ inc/functions.admin.php
@@ -11,18 +11,47 @@
  */
 
 
+// 1. Add Admin custom page for the theme
+// -----------------------------------------------------------------------------
+
 /**
  * add a custom admin page for the theme
  */
 function sunset_add_admin_page(){
+
     // add menu page
-    add_menu_page('Sunset Theme Options','Sunset','manage_options','sunset','sunset_create_admin_page', get_template_directory_uri().'/img/sunset-icon.png', 110);
+    add_menu_page(
+        'Sunset Theme Options', //Title
+        'Sunset', //Menu name shown
+        'manage_options', //Capability
+        'sunset_theme', //Page slug
+        'sunset_create_admin_page', // Callback function
+        get_template_directory_uri().'/img/sunset-icon.png', // Icon
+        110 //Order
+    );
 
     // add general menu page (is the same page as the main menu)
-    add_submenu_page('sunset','Sunset Theme Options','General','manage_options','sunset','sunset_create_admin_page');
+    add_submenu_page(
+        'sunset_theme', //Page
+        'Sunset Theme Options', //Title
+        'General', //Submenu name shown
+        'manage_options', //Capability
+        'sunset_theme', //menu slug
+        'sunset_create_admin_page' //Callback function
+    );
 
     // add css menu page
-    add_submenu_page('sunset','Sunset CSS Options','Custom CSS','manage_options','sunset-css','sunset_theme_css_setting');
+    add_submenu_page(
+        'sunset_theme', //Page
+        'Sunset CSS Options',//Title
+        'Custom CSS',//Submenu name shown
+        'manage_options', //Capability
+        'sunset_theme_css', //menu slug
+        'sunset_theme_css_setting' //Callback function
+    );
+
+    //activate custon Settings (Should wait for the menus to be created)
+    add_action('init_admin','sunset_custom_settings');
 }
 
 /**
@@ -36,6 +65,28 @@ function sunset_create_admin_page(){
  * generate setting page for the theme
  */
 function sunset_theme_css_setting(){}
+
+
+// 2. Add Custom setting for the theme
+// -----------------------------------------------------------------------------
+
+/**
+ * register the custom settings group on database
+ */
+function sunset_custom_settings(){
+
+}
+
+//
+function sunset_sidebar_options(){
+
+}
+
+function sunset_sidebar_name(){
+
+}
+
+// -----------------------------------------------------------------------------
 
 // load theme menu on admin wordpress hook
 add_action('admin_menu', 'sunset_add_admin_page');
--- inc/templates/sunset-admin.php
+++ inc/templates/sunset-admin.php
@@ -1,3 +1,3 @@
 <h1>Sunset Theme Options</h1>
-<h3 class="title">Manage Options</h3>
-<p>Customize the Default Wordpress Appearance Options</p>
+<form action="options.php" method="post">
+</form>
